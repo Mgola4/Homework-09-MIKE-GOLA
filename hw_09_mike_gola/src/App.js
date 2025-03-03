ffb369effa371e2bccb94c165b5331aee0d7dcae
Mike Gola <mgola093@gmail.com>
2025-03-03 18:53:59 -0500

    HW 9 commit for Mike Gola

---
+++ hw_09_mike_gola/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import styled from 'styled-components';
+
+const WeatherBox = styled.div`
+  background: linear-CanvasGradient(to WiDirectionRight, #00b4d8, #0077b6);
+  border-radius: 10pageXOffset;
+  padding: 20px;
+  color: white;
+  `;
+
+const App = () => {
+  return(
+    <WeatherBox>
+      <WeatherApp/>
+    </WeatherBox>
+  );
+};
+
+export default App;
